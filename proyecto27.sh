2e2358c426d916791b17c12258f59fdce364fec8
Evaristo GZ <evgarciaz@gmail.com>
2014-05-12 13:08:30 +0200

    Código comentado para facilitar su lectura

--- proyecto27.sh
+++ proyecto27.sh
@@ -5,11 +5,13 @@
 #anotando en un fichero de log los usuarios que no se han podido crear en el
 #caso de que ya existiesen.
 clear
+#Comprueba si el usuario es root.
 if [ `whoami` != "root" ]
 then
 	echo "Usted no es usuario root."
 	echo "Ejecute 'su' para identificarse y vuelva a ejecutar el script."
 else
+	#Comprueba la existencia del fichero log.
 	if [ -e fallidos.log ]
 	then
 		rm fallidos.log	
@@ -17,10 +19,12 @@ else
 	read -p "Introduzca el nombre base: " base
 	read -p "Introduzca el número de usuarios a crear: " nummax
 	numero='^[0-9]+$'
+	#Comprueba que se haya introducido un número.
 	if ! [[ $nummax =~ $numero ]]
 	then
 		echo "Debe de introducir un número de usuarios a crear."
 	else
+		#Repite el proceso el número de veces establecido.
 		for i in $(seq 1 $nummax)
 		do
 		useradd $base$i >> /dev/null 2>&1
@@ -33,6 +37,7 @@ else
 			fi
 		done
 	fi
+	#Comprueba si un fichero log ha sido creado.
 	if [ -e fallidos.log ]
 	then
 		echo -e "\nAlgunos usuarios ya existían. Consulte el fichero log con:"
