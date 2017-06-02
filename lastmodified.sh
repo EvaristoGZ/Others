a8b74e6f346b89daa8b88766b1c33325d13664bc
Evaristo GZ <evgarciaz@gmail.com>
2017-06-02 14:52:17 +0200

    Update lastmodified.sh

--- lastmodified.sh
+++ lastmodified.sh
@@ -3,6 +3,6 @@
 # Se ejecuta dentro del directorio a listar o se le pasa como parámetro.
 # sh lastmodified.sh o sh lastmodified.sh /ruta/del/archivo
 
-# Utilizado para migrar una instalación de Subversion de unos 100GB, manteniéndose en la instalación 
-# activa en producción y migrando de nuevo únicamente los repositorios modificados.
+#Utilizado para migrar una instalación de Subversion de unos 100GB, manteniéndose la instalación
+#de producción activa y migrando de nuevo únicamente los repositorios modificados.
 find $1 -type f -exec stat --format '%Y :%y %n' "{}" \; | sort -nr | cut -d: -f2- | head
